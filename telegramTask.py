8916599172d848c6be01a85e57f42fee916d3694
Alexander Skovpen <a.n.skovpen@gmail.com>
2016-08-25 13:54:25 +0300

    step

--- telegramTask.py
+++ telegramTask.py
@@ -23,6 +23,24 @@ class telegramTask(BaseTask):
       self.update_id = None
 
   def work(self):
+    print(self.bot.meteics)
     for update in self.tbot.getUpdates(offset=self.update_id, timeout=10):
       if update.message:
-        print(update.message.text)
+        if update.message.text=="/info":
+          res="test"
+  def _get_player_stats(self):
+    """
+    Helper method parsing the bot inventory object and returning the player stats object.
+    :return: The player stats object.
+    :rtype: dict
+    """
+    # TODO : find a better solution than calling the api
+    inventory_items = self.bot.api.get_inventory() \
+      .get('responses', {}) \
+      .get('GET_INVENTORY', {}) \
+      .get('inventory_delta', {}) \
+      .get('inventory_items', {})
+    return next((x["inventory_item_data"]["player_stats"]
+        for x in inventory_items
+        if x.get("inventory_item_data", {}).get("player_stats", {})),
+      None)
