67ef3349e7ede0695687bc0b1ff149d4f8527488
Alexander Skovpen <a.n.skovpen@gmail.com>
2016-08-25 12:52:55 +0300

    fix

--- telegramTask.py
+++ telegramTask.py
@@ -2,10 +2,15 @@ from pokemongo_bot.base_task import BaseTask
 
 class Task(BaseTask):
   SUPPORTED_TASK_API_VERSION = 1
+  api_key=""
+
+  def setup(self):
+    print("setup")
+    self.api_key=self.config.get('api_key')
 
   def initialize(self):
     print("init")
-    self.api.key=self.config.get('api_key')
+    self.api_key=self.config.get('api_key')
 
   def work(self):
-    print(self.api.key)
+    print(self.api_key)
