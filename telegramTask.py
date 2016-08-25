acd28f07551e10aee3fc03bbe07350e61f80f518
Alexander Skovpen <a.n.skovpen@gmail.com>
2016-08-25 13:00:35 +0300

    step

--- telegramTask.py
+++ telegramTask.py
@@ -4,13 +4,14 @@ class Task(BaseTask):
   SUPPORTED_TASK_API_VERSION = 1
   api_key=""
 
-  def setup(self):
-    print("setup")
-    self.api_key=self.config.get('api_key')
-
   def initialize(self):
     print("init")
     self.api_key=self.config.get('api_key')
+    if api_key==None:
+      self.emit_event(
+        'api_error',
+        formatted='api_key not defined.'
+      )
 
   def work(self):
     print(self.api_key)
